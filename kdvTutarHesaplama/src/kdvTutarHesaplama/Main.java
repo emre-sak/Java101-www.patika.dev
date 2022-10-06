53b38b5d734c0c20f0cb1e4466ed6d30eb4507be
Emre Sak <emresakmd5@gmail.com>
2022-10-06 16:37:33 +0300

    www.patika.dev Java101 Odev
    www.patika.dev Java101 Odev

---
+++ kdvTutarHesaplama/src/kdvTutarHesaplama/Main.java
@@ -0,0 +1,27 @@
+package kdvTutarHesaplama;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Tutarı Giriniz : ");
+		double tutar = scanner.nextInt();
+
+		double kdv = 0.18;
+		double kdv2 = 0.08;
+
+		boolean girilenTutar = (tutar > 0 && tutar < 1000);
+
+		double kdvTutari = girilenTutar ? tutar * kdv : tutar * kdv2;
+
+		System.out.println("Tutar : " + tutar + " TL");
+		System.out.println("KDV tutarı : " + kdvTutari + " TL");
+		System.out.println("KDV ile birlikte toplam tutar : " + (tutar + kdvTutari) + " TL");
+
+	}
+
+}
