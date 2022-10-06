848a40ff0d13ba7b77f61b36afd460f25d96b1b2
Emre Sak <emresakmd5@gmail.com>
2022-10-06 19:07:37 +0300

    www.patika.dev Java101 Odev
    www.patika.dev Java101 Odev

---
+++ manavKasaProgrami/src/manavKasaProgrami/Main.java
@@ -0,0 +1,34 @@
+package manavKasaProgrami;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		double armut = 2.14;
+		double elma = 3.67;
+		double domates = 1.11;
+		double muz = 0.95;
+		double patlican = 5.00;
+		
+		System.out.print("Armut Kaç Kilo ? : ");
+		double kg1 = scanner.nextDouble();
+		System.out.print("Elma Kaç Kilo ? : ");
+		double kg2 = scanner.nextDouble();
+		System.out.print("Domates Kaç Kilo ? : ");
+		double kg3 = scanner.nextDouble();
+		System.out.print("Muz Kaç Kilo ? : ");
+		double kg4 = scanner.nextDouble();
+		System.out.print("Patlican Kaç Kilo ? : ");
+		double kg5 = scanner.nextDouble();
+		
+		double hesapla = (kg1 * armut ) + (kg2 * elma) + (kg3 * domates) + (kg4 * muz) + (kg5 * patlican);
+		
+		System.out.println("Toplam Tutar : " + hesapla + " TL");
+		
+	}
+
+}
