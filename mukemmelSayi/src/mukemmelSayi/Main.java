c3f23794a7137f3c591079552fe059769beaf449
Emre Sak <emresakmd5@gmail.com>
2022-10-17 00:37:07 +0300

    www.patika.dev Java101 Odev
    www.patika.dev Java101 Odev

---
+++ mukemmelSayi/src/mukemmelSayi/Main.java
@@ -0,0 +1,33 @@
+package mukemmelSayi;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("1. Sayıyı giriniz : ");
+		int number = scanner.nextInt();
+
+		int total = 0;
+
+		for (int i = 1; i < number; i++) {
+
+			if (number % i == 0) {
+				total += i;
+			}
+
+		}
+		
+
+		if(number == total) {
+			System.out.println(number + " mükemmel sayıdır.");
+		}else {
+			System.out.println(number + " mükemmel sayı değildir.");
+		}
+
+	}
+
+}
