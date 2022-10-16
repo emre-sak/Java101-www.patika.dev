50ff1f6b6be3582a91bfbcd947652d71a31f6b69
Emre Sak <emresakmd5@gmail.com>
2022-10-16 19:46:23 +0300

    www.patika.dev Java101 Odev
    www.patika.dev Java101 Odev

---
+++ harmonikSeri/src/harmonikSeri/Main.java
@@ -0,0 +1,24 @@
+package harmonikSeri;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Sayıyı giriniz : ");
+		int n = scanner.nextInt();
+		
+		double result = 0;
+		
+		for(double i = 1; i <= n; i++) {
+			result += (1 / i);
+		}
+		
+		System.out.println(result);
+
+	}
+
+}
