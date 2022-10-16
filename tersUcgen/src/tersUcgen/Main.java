726f8aeec35120ed4835bdbd1d3912a2ccaa8322
Emre Sak <emresakmd5@gmail.com>
2022-10-17 00:42:45 +0300

    www.patika.dev Java101 Odev
    www.patika.dev Java101 Odev

---
+++ tersUcgen/src/tersUcgen/Main.java
@@ -0,0 +1,29 @@
+package tersUcgen;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Basamak Sayısını Giriniz : ");
+		int n = scanner.nextInt();
+
+		for (int i = n; i >= 1; i--) {
+			for (int k = 1; k <= (n - i); k++) {
+				System.out.print(" ");
+			}
+
+			for (int j = 1; j <= (2 * i) - 1; j++) {
+				System.out.print("*");
+			}
+
+			System.out.println();
+
+		}
+
+	}
+
+}
